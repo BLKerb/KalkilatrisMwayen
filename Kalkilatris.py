f60e8e7a693568006e39534a3284743f79de8b2b
BLKerb <141364060+BLKerb@users.noreply.github.com>
2023-08-11 22:09:54 -0400

    Kalkilatris.py

--- Kalkilatris.py
+++ Kalkilatris.py
@@ -8,20 +8,24 @@ for i in range(kantite_not):
 Kantite=sum(lis_not)
 mwayen= Kantite/kantite_not
 
-print ("Men lis not yo :", lis_not)
+print ("Men lis not yo: ", lis_not)
 
 if (mwayen>=90):
     print ("\nE mwayenn not yo se: ", mwayen)
     print ("\nA")
+    
 elif(mwayen>=80 and mwayen<90):
     print ("E mwayenn not yo se: ", mwayen)
     print ("\nB")
+    
 elif(mwayen>=70 and mwayen<80):
     print ("E mwayenn not yo se: ", mwayen)
     print ("\nC")
+    
 elif(mwayen>=60 and mwayen<70):
     print ("E mwayenn not yo se: ", mwayen)
     print ("\nD")
+    
 else:
     print ("E mwayenn not yo se: ", mwayen)
     print ("\nF")
